80f42f679bee429f5a0a6b26a4b5fdad5981ab47
Aaron Fan <fanyu1980@hotmail.com>
2019-08-17 21:09:13 +1000

    add tutorial link

--- DB_Initiation.py
+++ DB_Initiation.py
@@ -36,14 +36,4 @@ conn.commit()
 
 conn.close()
 
-# c = conn.cursor()
-#
-# c.execute(''' CREATE TABLE stocks1(
-# date text, trans text, symbol text, qty real, price real
-# ) ''')
-#
-# c.execute("INSERT INTO stocks1 VALUES('2006-01-05', 'BUY', 'RHAT', 100, 35.14)")
-#
-# conn.commit()
-#
-# conn.close()
+# https://www.pythoncentral.io/introduction-to-sqlite-in-python/
